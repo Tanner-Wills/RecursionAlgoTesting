4c49b2cf9508fd5c612be94f1a89d735e256bb1d
Tanner-Wills <tannerwills1@gmail.com>
2021-08-03 15:39:05 -0400

    updated while loop 2 pointer

--- src/IncreasingSubsequence.java
+++ src/IncreasingSubsequence.java
@@ -6,7 +6,7 @@ public class IncreasingSubsequence {
 
     public static void main(String[] args) {
         //instance variables
-        List<Integer> myList = new ArrayList<>(Arrays.asList(1,7,3,5,2,8,10,24,-1,-5,4));
+        List<Integer> myList = new ArrayList<>(Arrays.asList(1, 7, 3, 5, 2, 8, 10, 24, -1, -5, 4));
 
         int subcount = myList.size();
         int start = 0;
@@ -15,32 +15,34 @@ public class IncreasingSubsequence {
         int pointer1 = 0;
         int pointer2 = 1;
         //Iterate through the ArrayList
-        while(start < myList.size()-2){
-            System.out.println("hooo" + start);
+        while (start < myList.size() - 2) {
+            List<Integer> sublist = myList.subList(start, end + 1);
 
-            while(end < myList.size()){
-
-                List<Integer> sublist = myList.subList(start,end+1);
+            while (pointer2 < myList.size()) {
 
                 //if the last two elements are increasing
-                if(sublist.get(pointer1) < sublist.get(pointer2)){
-
+                if (myList.get(pointer1) < myList.get(pointer2)) {
                     pointer1 = pointer2;
                     pointer2 += 1;
-                    end += 1;
                     subcount += 1;
 
                     //if the last two elements are decreasing
                 } else {
                     pointer2 += 1;
-                    end += 1;
                 }
 
             }
-            start += 1;
-            end = start + 2;
-            pointer1 = 0;
-            pointer2 = 1;
+            if (end < myList.size() - 1) {
+                end += 1;
+                pointer1 = 0;
+                pointer2 = sublist.size() - 1;
+
+            } else {
+                start += 1;
+                end = start + 2;
+                pointer1 = 0;
+                pointer2 = 1;
+            }
 
         }
         System.out.println(subcount);
