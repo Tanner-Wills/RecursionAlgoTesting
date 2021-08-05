2ae3dbcb13cb9194cad81802b026a3eb360a2f60
Tanner-Wills <tannerwills1@gmail.com>
2021-08-05 11:06:44 -0400

    added additional while loop to prevent re-counting the same subarray

--- src/IncreasingSubsequence.java
+++ src/IncreasingSubsequence.java
@@ -10,46 +10,52 @@ public class IncreasingSubsequence {
 
         int subcount = myList.size();
         int start = 0;
-        int end = 2;
+        int end = 1;
 
-        int pointer1 = 0;
-        int pointer2 = 1;
+        int pointer1 = start;
+        int pointer2 = end;
 
         //Iterate through the ArrayList
-        while (start < myList.size() - 2) {
-
+        while (start < myList.size() - 1) {
             System.out.println("*****");
-
             while (pointer2 < myList.size()) {
-
                 //if the last two elements are increasing
                 if (myList.get(pointer1) < myList.get(pointer2)) {
 
                     System.out.println(myList.get(pointer1));
                     System.out.println(myList.get(pointer2));
-                    System.out.println("/////////");
-                    System.out.println("Count = " + subcount);
-                    System.out.println("/////////");
 
                     pointer1 = pointer2;
                     pointer2 += 1;
                     subcount += 1;
 
+                    System.out.println("/////////");
+                    System.out.println("Count = " + subcount);
+                    System.out.println("pointer 2 = " + pointer2);
+                    System.out.println("end = " + end);
+                    System.out.println("/////////");
                     //if the last two elements are decreasing
                 } else {
                     pointer2 += 1;
                 }
             }
+            // increment "end" position and set pointer 2 equal to it
+            // this will increment the pointer 1 and 2 start position to identify all possible subarrays from the start position
             if (end < myList.size()-1) {
+                end += 1;
                 pointer1 = start;
                 pointer2 = end;
-                end += 1;
-
+            // If all possible subarrays have been examined from the current start position, increment start position +1
             } else {
                 start += 1;
-                end = start + 2;
+                end = start + 1;
                 pointer1 = start;
                 pointer2 = start + 1;
+            }
+            //while loop to prevent duplicate increasing subarrays
+            while(pointer2 < myList.size()-1 && myList.get(pointer1) > myList.get(pointer2)){
+                end += 1;
+                pointer2 = end;
             }
         }
         System.out.println(subcount);
